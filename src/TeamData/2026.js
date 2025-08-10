ac4177a5c3105890249f851bf4d0d5197c5af7e2
Mahesh Kalal <maheshkalal9390@gmail.com>
2025-08-11 00:58:54 +0530

    2nd

--- src/TeamData/2026.js
+++ src/TeamData/2026.js
@@ -35,5 +35,29 @@ export const batch2026 = [
       email: "emma.thompson@university.edu",
       insta: "https://instagram.com/emmathompson"
     }
+  },
+  {
+    id: 16,
+    name: "Emma ",
+    role: "General Secretary",
+
+    image: "https://images.unsplash.com/photo-1594736797933-d0401ba2fe65?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80",
+    social: {
+      linkedin: "https://linkedin.com/in/emmathompson",
+      email: "emma.thompson@university.edu",
+      insta: "https://instagram.com/emmathompson"
+    }
+  },
+  {
+    id: 66,
+    name: "Emma ",
+    role: "General Secretary",
+
+    image: "https://images.unsplash.com/photo-1594736797933-d0401ba2fe65?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80",
+    social: {
+      linkedin: "https://linkedin.com/in/emmathompson",
+      email: "emma.thompson@university.edu",
+      insta: "https://instagram.com/emmathompson"
+    }
   }
 ];
