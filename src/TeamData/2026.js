e79b8630dd08bdd281f5f33e91d49bbdceb1ae27
sangam saurav <sangamsh04@gmail.com>
2025-08-10 17:48:58 +0530

    updating teams section-2

--- src/TeamData/2026.js
+++ src/TeamData/2026.js
@@ -26,7 +26,7 @@ export const batch2026 = [
   },
   {
     id: 6,
-    name: "Emma Thompson",
+    name: "Emma ",
     role: "General Secretary",
 
     image: "https://images.unsplash.com/photo-1594736797933-d0401ba2fe65?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80",
